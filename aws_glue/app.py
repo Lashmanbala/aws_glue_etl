f2152a3ff3a721379ec1142c27ea702845673e31
lashmanbala <lashmanbala@gmail.com>
2024-10-23 11:48:38 +0530

    Glue job created and started

--- aws_glue/app.py
+++ aws_glue/app.py
@@ -1,21 +1,47 @@
 from iam_util import create_iam_policy, create_iam_role
-from glue_util import create_glue_crawler, start_glue_crawler
+from glue_util import create_glue_crawler, start_glue_crawler, create_glue_job, run_glue_job
 import dotenv
 
 dotenv.load_dotenv()
 
 bucket_name = 'github-activity-bucket'
 
-policy_ARN = create_iam_policy(bucket_name)
+# policy_ARN = create_iam_policy(bucket_name)
 
-res = create_iam_role(policy_ARN)
-role_ARN = res['Role']['Arn']
-print(res)
+# res = create_iam_role(policy_ARN)
+# role_ARN = res['Role']['Arn']
+# print(res)
 
-# role_ARN = 'arn:aws:iam::872515260721:role/service-role/AWSGlueServiceRole-GHactivity'
+# crawler_NAME = 'gha_crawler'
+# s3_path = f's3://{bucket_name}/landing/'
+db_name = 'gha_database'
+# prefix = 'gha_' 
 
-crawler_NAME = 'gha_crawler'
-s3_path = f's3://{bucket_name}/landing/'
+# res = create_glue_crawler(crawler_NAME, role_ARN, db_name, s3_path, prefix)
+# print(res)
+
+# start_res = start_glue_crawler(crawler_NAME)
+# print(start_res)
+
+# job_name = 'gha-job'
+role_ARN = 'arn:aws:iam::872515260721:role/service-role/AWSGlueServiceRole-GHactivity'
+# script_location = f's3://{bucket_name}/scripts/glue_script.py'  # S3 path for the Glue script
+# temp_dir = f's3://{bucket_name}/temp/'  # Temporary directory for Glue
+
+# res = create_glue_job(job_name, role_ARN, script_location, temp_dir)
+
+# print(res)
+
+# # table_name = f'{prefix}landing'
+# table_name = 'gha_landing_landing'
+# folder_name = 'cleaned_parquet'
+
+# run_response = run_glue_job(job_name, db_name, table_name, bucket_name, folder_name)
+# print(run_response)
+
+
+crawler_NAME = 'gha_parquet_crawler'
+s3_path = f's3://{bucket_name}/cleaned_parquet/'
 db_name = 'gha_database'
 prefix = 'gha_' 
 
--- aws_glue/glue_util.py
+++ aws_glue/glue_util.py
@@ -1,9 +1,10 @@
 import boto3
+import time
 
 def create_glue_crawler(crawler_name, role, database_name, s3_target_path, table_prefix):
     glue_client = boto3.client('glue')
 
-    glue_client.create_crawler(
+    res = glue_client.create_crawler(
             Name=crawler_name,
             Role=role,
             DatabaseName=database_name,
@@ -22,8 +23,56 @@ def create_glue_crawler(crawler_name, role, database_name, s3_target_path, table
             # , Schedule='cron(0 12 * * ? *)',  # Optional schedule for periodic runs
 
             )
+    time.sleep(5) # wait untill crawler created
+    return res
 
 def start_glue_crawler(crawler_name):
     glue_client = boto3.client('glue')
 
-    glue_client.start_crawler(Name=crawler_name)
+    res = glue_client.start_crawler(Name=crawler_name)
+
+    return res
+
+def create_glue_job(job_name, iam_role, script_location, temp_dir):
+    glue_client = boto3.client('glue')
+
+    response = glue_client.create_job(
+        Name=job_name,
+        Role=iam_role,
+        Command={
+            'Name': 'glueetl',  # Specifies this is a Glue ETL job
+            'ScriptLocation': script_location,
+            'PythonVersion': '3'  # Python version (2 or 3)
+        },
+        GlueVersion='4.0',  # Optional: Glue version
+        # MaxCapacity=max_capacity,  # Specifies the number of DPUs to allocate 
+        # Need not to set max capacity if n.of workers and worker type is set
+        NumberOfWorkers=4,
+        WorkerType='Standard',
+        DefaultArguments={
+            '--TempDir': temp_dir,
+            '--job-bookmark-option': 'job-bookmark-enable',
+        },
+        Description='github activity data transformation job',
+        ExecutionProperty={
+            'MaxConcurrentRuns': 1  # Limits the job to one concurrent run
+        },
+        Timeout=60,  # Timeout in minutes
+        MaxRetries=1,  # Retry number # Meaning totally 2 retries: retry-0 and retry-1.
+    )
+    time.sleep(5) # wait untill job created
+
+    return response
+
+def run_glue_job(job_name, database_name, table_name, bucket_name, folder_name):
+    glue_client = boto3.client('glue')
+
+    res = glue_client.start_job_run(JobName=job_name,
+                                       Arguments={                # Setting up env variables
+                                     '--DATABASE_NAME': f'{database_name}',
+                                    '--TABLE_NAME': f'{table_name}',
+                                    '--TGT_BUCKET_NAME':  f'{bucket_name}',
+                                    '--TGT_FOLDER_NAME': F'{folder_name}'
+                                    })
+
+    return res
