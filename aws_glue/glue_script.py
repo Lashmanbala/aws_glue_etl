c8b0a934806f66b1e39e41dd8dbef09729e11533
lashmanbala <lashmanbala@gmail.com>
2024-10-28 11:31:07 +0530

    Reading data directly from s3

--- aws_glue/glue_script.py
+++ aws_glue/glue_script.py
@@ -8,7 +8,7 @@ from awsglue.job import Job
 from pyspark.sql.functions import date_format, substring
 from awsglue.dynamicframe import DynamicFrame
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'DATABASE_NAME', 'TABLE_NAME', 'TGT_BUCKET_NAME', 'TGT_FOLDER_NAME'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'SRC_BUCKET_NAME', 'SRC_FOLDER_NAME', 'TGT_BUCKET_NAME', 'TGT_FOLDER_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -16,19 +16,41 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Getting variables
-database_name = args['DATABASE_NAME']
-table_name = args['TABLE_NAME']
+src_bucket_name = args['SRC_BUCKET_NAME']
+src_folder_name = args['SRC_FOLDER_NAME']
 tgt_bucket_name = args['TGT_BUCKET_NAME']
 tgt_folder_name = args['TGT_FOLDER_NAME']
 
-# Reading data from glue table as dynamic_frame
-datasource = glueContext.create_dynamic_frame.from_catalog(database = database_name,  # should be named args not positional args
-                                                           table_name = table_name, 
-                                                           transformation_ctx="datasource")
+# Reading data from s3
+datasource_s3 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "true"}, connection_type="s3", 
+                                                              format="json", connection_options={"paths": [f"s3://{src_bucket_name}/{src_folder_name}/"], 
+                                                                                                 "recurse": True}, transformation_ctx="datasource_s3")
 
-# Changing Schema
-ChangeSchema_datasource = ApplyMapping.apply(frame=datasource, 
-                                             mappings=[("id", "string", "id", "string"), ("type", "string", "type", "string"), ("actor.id", "int", "actor.id", "int"), ("actor.login", "string", "actor.login", "string"), ("actor.display_login", "string", "actor.display_login", "string"), ("actor.gravatar_id", "string", "actor.gravatar_id", "string"), ("actor.url", "string", "actor.url", "string"), ("actor.avatar_url", "string", "actor.avatar_url", "string"), ("repo.id", "int", "repo.id", "int"), ("repo.name", "string", "repo.name", "string"), ("repo.url", "string", "repo.url", "string"), ("payload.repository_id", "int", "payload.repository_id", "int"), ("payload.push_id", "long", "payload.push_id", "long"), ("payload.size", "int", "payload.size", "int"), ("payload.distinct_size", "int", "payload.distinct_size", "int"), ("payload.ref", "string", "payload.ref", "string"), ("payload.head", "string", "payload.head", "string"), ("payload.before", "string", "payload.before", "string"), ("payload.commits", "array", "payload.commits", "array"), ("payload.action", "string", "payload.action", "string"), ("payload.ref_type", "string", "payload.ref_type", "string"), ("payload.master_branch", "string", "payload.master_branch", "string"), ("payload.description", "string", "payload.description", "string"), ("payload.pusher_type", "string", "payload.pusher_type", "string"), ("payload.number", "int", "payload.number", "int"), ("payload.pages", "array", "payload.pages", "array"), ("public", "boolean", "public", "boolean"), ("created_at", "string", "created_at", "string"), ("org.id", "int", "org.id", "int"), ("org.login", "string", "org.login", "string"), ("org.gravatar_id", "string", "org.gravatar_id", "string"), ("org.url", "string", "org.url", "string"), ("org.avatar_url", "string", "org.avatar_url", "string")], transformation_ctx="ChangeSchema_datasource")
+
+# Script generated for node Change Schema
+ChangeSchema_datasource = ApplyMapping.apply(frame=datasource_s3, 
+                                             mappings=[("id", "string", "id", "string"), ("type", "string", "type", "string"), 
+                                                       ("actor.id", "int", "actor.id", "int"), ("actor.login", "string", "actor.login", "string"), 
+                                                       ("actor.display_login", "string", "actor.display_login", "string"), 
+                                                       ("actor.gravatar_id", "string", "actor.gravatar_id", "string"), 
+                                                       ("actor.url", "string", "actor.url", "string"), 
+                                                       ("actor.avatar_url", "string", "actor.avatar_url", "string"), ("repo.id", "int", "repo.id", "int"),
+                                                         ("repo.name", "string", "repo.name", "string"), ("repo.url", "string", "repo.url", "string"),
+                                                           ("payload.ref", "string", "payload.ref", "string"), 
+                                                           ("payload.ref_type", "string", "payload.ref_type", "string"), 
+                                                           ("payload.master_branch", "string", "payload.master_branch", "string"), 
+                                                           ("payload.description", "string", "payload.description", "string"), 
+                                                           ("payload.pusher_type", "string", "payload.pusher_type", "string"), 
+                                                           ("payload.action", "string", "payload.action", "string"), ("payload.repository_id", "int", "payload.repository_id", "int"),
+                                                             ("payload.push_id", "long", "payload.push_id", "long"), 
+                                                           ("payload.size", "int", "payload.size", "int"), ("payload.distinct_size", "int", "payload.distinct_size", "int"), 
+                                                           ("payload.head", "string", "payload.head", "string"), ("payload.before", "string", "payload.before", "string"),
+                                                             ("payload.commits", "array", "payload.commits", "array"), ("payload.number", "int", "payload.number", "int"), 
+                                                             ("payload.pages", "array", "payload.pages", "array"), ("public", "boolean", "public", "boolean"), 
+                                                             ("created_at", "string", "created_at", "string"), ("org.id", "int", "org.id", "int"), ("org.login", "string", "org.login", "string"),
+                                                               ("org.gravatar_id", "string", "org.gravatar_id", "string"), ("org.url", "string", "org.url", "string"), 
+                                                               ("org.avatar_url", "string", "org.avatar_url", "string")], 
+                                                               transformation_ctx="ChangeSchema_datasource")
 
 # Creating columns for partitioning with changing dynamic_frame into dataframe
 df = ChangeSchema_datasource. \
@@ -41,10 +63,11 @@ df = ChangeSchema_datasource. \
 dyf = DynamicFrame.fromDF(dataframe=df, glue_ctx=glueContext, name="dyf")
 
 # writing partitioned data into s3 in Parquet format
-dyf_partitioned = glueContext.write_dynamic_frame.from_options(frame=dyf, 
-        connection_type="s3", 
-        format="glueparquet", 
-        connection_options={"path": f"s3://{tgt_bucket_name}/{tgt_folder_name}/", "partitionKeys": ["year", "month", "day"]}, 
-        format_options={"compression": "snappy"}, transformation_ctx="dyf_partitioned")
+dyf_partitioned = glueContext.write_dynamic_frame.from_options(frame=dyf, connection_type="s3", format="glueparquet", 
+                                                               connection_options={"path": f"s3://{tgt_bucket_name}/{tgt_folder_name}/", 
+                                                                                   "partitionKeys": ["year", "month", "day"]}, 
+                                                              format_options={"compression": "snappy"}, 
+                                                              transformation_ctx="dyf_partitioned")
+
 
 job.commit()
